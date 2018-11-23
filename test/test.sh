64d3d38c12456148d8fa9c05dd31383485e7aa4a
Shawn <shawnw.mobile@gmail.com>
2018-11-22 21:29:32 -0800

    Add test cases for argument parsing.

--- test/test.sh
+++ test/test.sh
@@ -1,4 +1,77 @@
 #!/bin/sh
 
+
+../build/kv -f put test.db k v 2>/dev/null
+if [ $? -ne 1 ]; then
+    echo "test invalid argument 1 failed"
+    exit 1
+fi
+
+../build/kv -b foo put test.db k v 2>/dev/null
+if [ $? -ne 1 ]; then
+    echo "test invalid argument 2 failed"
+    exit 1
+fi
+
+../build/kv nocommand test.db 2>/dev/null
+if [ $? -ne 1 ]; then
+    echo "test invalid argument 3 failed"
+    exit
+fi
+
+../build/kv 2>/dev/null
+if [ $? -ne 1 ]; then
+    echo "test missing argument 1 failed"
+    exit 1
+fi
+
+../build/kv put 2>/dev/null
+if [ $? -ne 1 ]; then
+    echo "test missing argument 2 failed"
+    exit 1
+fi
+
+../build/kv put test.db 2>/dev/null
+if [ $? -ne 1 ]; then
+    echo "test missing argument 3 failed"
+    exit 1
+fi
+
+../build/kv put test.db key0 2>/dev/null
+if [ $? -ne 1 ]; then
+    echo "test missing argument 4 failed"
+    exit 1
+fi
+
+../build/kv get 2>/dev/null
+if [ $? -ne 1 ]; then
+    echo "test missing argument 5 failed"
+    exit 1
+fi
+
+../build/kv get test.db 2>/dev/null
+if [ $? -ne 1 ]; then
+    echo "test missing argument 6 failed"
+    exit 1
+fi
+
+../build/kv delete 2>/dev/null
+if [ $? -ne 1 ]; then
+    echo "test missing argument 7 failed"
+    exit 1
+fi
+
+../build/kv delete test.db 2>/dev/null
+if [ $? -ne 1 ]; then
+    "echo test missing argument 8 failed"
+    exit 1
+fi
+
+../build/kv list 2>/dev/null
+if [ $? -ne 1 ]; then
+    echo "test missing argument 9 failed"
+    exit 1
+fi
+
 ./test1.sh sqlite
 ./test1.sh gdbm
